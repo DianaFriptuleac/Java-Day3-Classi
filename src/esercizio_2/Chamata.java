f727f248c6c002473da178f8d10d80699a7be153
Diana <dianadorojuc3@gmail.com>
2024-09-25 15:56:08 +0200

    Ex 2 class Chiamata

---
+++ src/esercizio_2/Chamata.java
@@ -0,0 +1,25 @@
+package esercizio_2;
+
+public class Chamata {
+    private String numeroChiamato;
+    private int durata;
+
+    public Chiamata( String numeroChiamato, int durata){
+        this.numeroChiamato = numeroChiamato;
+        this.durata = durata;
+    }
+    //Getter per prendere la chiamata
+    public String getNumeroChiamato(){
+        return numeroChiamato;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    @Override
+    public String toString(){
+        return "Il numero chiamato: " + numeroChiamato + " con la durata di " + durata + " minuti";
+    }
+}
+
