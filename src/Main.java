de10bcf108fcb74b50f6934f6774acf31f33b4c1
Diana <dianadorojuc3@gmail.com>
2024-09-25 18:09:40 +0200

    Ex 3 finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 import esercizio_1.Rettangolo;
 import esercizio_2.SIM;
+import esercizio_3.Articolo;
+import esercizio_3.Carrello;
+import esercizio_3.Cliente;
+
+import javax.swing.text.Caret;
+import java.sql.SQLSyntaxErrorException;
 
 public class Main {
     public static void main(String[] args) {
@@ -34,5 +40,34 @@ public class Main {
 
         newSim.stampaDatiSIM();
 
+
+        System.out.println("-----------------Ex3---------------------");
+
+        //Creo clienti
+        Cliente cliente1 = new Cliente(125555412, "Mario Rossi", "mariorossii@gmail.com", "01/10/2023");
+        Cliente cliente2 = new Cliente(245512555, "Maria Neri", "marianeri@gmail.com", "12/08/2024");
+
+        //Creo un carello per cliente
+        Carrello carrello1 = new Carrello(cliente1.getFullName());
+        Carrello carrello2 = new Carrello(cliente2.getFullName());
+
+        //Creo articoli
+        Articolo articolo1 = new Articolo(125, "Samsung S24+", 950.00);
+        Articolo articolo2 = new Articolo(355, "Samsung S24 Ultra", 1050.00);
+        Articolo articolo3 = new Articolo(15525, "IPhone", 1250.00);
+
+        //Add articoli nel carello
+        carrello1.addArticolo(articolo1);
+        carrello1.addArticolo(articolo2);
+
+        carrello2.addArticolo(articolo3);
+
+        //Stampo i dettagli
+        System.out.println(cliente1);
+        System.out.println(cliente2);
+
+        //Stampo il carello
+        carrello1.dettagliCarello();
+        carrello2.dettagliCarello();
     }
 }
--- src/esercizio_3/Carrello.java
+++ src/esercizio_3/Carrello.java
@@ -9,10 +9,27 @@ public class Carrello {
 
     //Costruttore
 
-    public Carrello(String clienteAssociato, ArrayList articoli, double totaleCosto){
+    public Carrello(String clienteAssociato){
         this.clienteAssociato= clienteAssociato;
         this.articoli = new ArrayList<>(); //inizzializzo con un array vuoto
         this.totaleCosto = 0.0; //inizializzo a zero
+    }
+
+    //Metodo per aggiungere articolo al carello
+    public void addArticolo(Articolo newarticolo){
+        articoli.add(newarticolo);
+        totaleCosto += newarticolo.getPrezzo();
+    }
+
+    //Metodo per stampare il carello
+    public void dettagliCarello(){
+        System.out.println("Cliente" + clienteAssociato);
+        System.out.println("Articoli nel carello: ");
+        for(Articolo art : articoli){
+            System.out.println(art);
+        }
+
+        System.out.println("Totale: " + totaleCosto + " euro");
     }
 
 }
